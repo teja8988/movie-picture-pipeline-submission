0a515b6362a3f9cfba1e3fa886ce62298a1a2f8d
teja8988 <saitejaparri2003@gmail.com>
2026-09-07 13:28:53 +0530

    Fix ESLint config

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -1,4 +1,3 @@
-```javascript
 module.exports = {
   ignorePatterns: ['build/', 'dist/', 'node_modules/'],
   extends: ['eslint:recommended', 'plugin:react/recommended'],
@@ -35,4 +34,3 @@ module.exports = {
     'react/jsx-uses-vars': 'warn',
   },
 };
-```
